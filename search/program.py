deeb06222bc7440c6b9874f23ea87260f9f2ebfb
FG <quannguyen@student.unimelb.edu.au>
2025-03-27 23:26:50 +1100

    implemented recursive moves

--- search/program.py
+++ search/program.py
@@ -5,43 +5,48 @@ from .core import CellState, Coord, Direction, MoveAction
 from .utils import render_board
 from collections import deque
 
+BOARD_N = 8
 
-def generate_jump_moves(coord: Coord, board: dict[Coord, CellState], moves):
+def generate_jump_moves(coord: Coord, board: dict[Coord, CellState], visited: set[Coord], path, start_coord, moves):
+    if coord in visited:
+        return
+    
+    visited.add(coord)
     directions = [Direction.Down, Direction.DownLeft, Direction.DownRight, Direction.Left, Direction.Right]
 
     for direc in directions:
-        next_square = coord + direc
-        next_next_square = next_square + direc
-        
-        # if abs(next_square.c - coord.c) > 1 or next_square not in board:
-        #     continue
-        # if abs(next_next_square.c - next_square.c) > 1 or \
-        #     abs(next_next_square.r - next_square.r) > 1 or \
-        #     next_next_square not in board:
-        #     continue
+        try:
+            next_square = coord + direc
+            next_next_square = next_square + direc
+        except:
+            continue
 
         if next_square not in board or next_next_square not in board:
             continue
 
-        if board[next_square] == CellState.BLUE and board[next_next_square] == CellState.LILY_PAD:
-            if MoveAction(coord, ) not in moves:
-                moves.append(MoveAction(coord, new))
+        if board[next_square] == CellState.BLUE and board[next_next_square] == CellState.LILY_PAD \
+            and next_next_square not in visited:
+            
+            new_path = path + [direc]
+            moves.append((MoveAction(coord=start_coord, _directions=new_path), next_next_square))
+            generate_jump_moves(next_next_square, board, visited, new_path, start_coord, moves)
 
-                generate_jump_moves(next_next_square, board, moves)
-
-
-def generate_moves(coord: Coord, board: dict[Coord, CellState]):
+def generate_moves(coord: Coord, board: dict[Coord, CellState]) -> list[(MoveAction, Coord)]:
     directions = [Direction.Down, Direction.DownLeft, Direction.DownRight, Direction.Left, Direction.Right]
     moves = []
     for direc in directions:
-        next_square = coord + direc
-        if abs(next_square.c - coord.c) > 1 or next_square not in board:
+        try:
+            next_square = coord + direc
+        except:
+            continue
+
+        if next_square not in board:
             continue
 
         if board[next_square] == CellState.LILY_PAD:
-            moves.append(MoveAction(coord, direc))
+            moves.append( (MoveAction(coord=coord, _directions=[direc]), next_square) )
     
-    generate_jump_moves(coord, board, moves)
+    generate_jump_moves(coord, board, set(), [], coord, moves)
 
     return moves
 
@@ -71,46 +76,39 @@ def search(
 
     q = deque([start])
     visited = {start,}
-    dist = {start: 0}
     previous = {start: None}
     final_coord = None
 
     while len(q) > 0:
         current_coord = q.popleft()
-        board_copy = board.copy()
-        del board_copy[start]
-        board_copy[current_coord] = CellState.RED
-        # print(render_board(board_copy, ansi=True))
-        # print(dist[current_coord])
 
-
-        if current_coord.r == 7:
+        if current_coord.r == BOARD_N-1:
             final_coord = current_coord
             break
         
-        for move in generate_moves(current_coord, board):
-            next_square = current_coord + move._directions
-            if next_square not in visited:
-                q.append(next_square)
-                visited.add(next_square)
-                dist[next_square] = dist[current_coord] + 1
-                previous[next_square] = current_coord
-        
-    L = [final_coord]
-    while previous[L[-1]] != None:
-        L.append(previous[L[-1]])
+        for move, dest in generate_moves(current_coord, board):
+            if dest not in visited:
+                q.append(dest)
+                visited.add(dest)
+                previous[dest] = (current_coord, move)
+    
+    # print(render_board(board, ansi=True))
 
-    for current_coord in L[::-1]:
-        board_copy = board.copy()
-        del board_copy[start]
-        board_copy[current_coord] = CellState.RED
-        print(render_board(board_copy, ansi=True))
-        print(dist[current_coord])
+    if not final_coord:
+        return None
+
+    L = [final_coord]
+    final_moves = []
+    while previous[L[-1]] != None:
+        prev_coord, move = previous[L[-1]]
+        L.append(prev_coord)
+        final_moves.append(move)
+    
+    return list(reversed(final_moves))
 
     # The render_board() function is handy for debugging. It will print out a
     # board state in a human-readable format. If your terminal supports ANSI
     # codes, set the `ansi` flag to True to print a colour-coded version!
-    print(render_board(board, ansi=True))
 
     # Do some impressive AI stuff here to find the solution...
     # ...
@@ -121,14 +119,4 @@ def search(
     # output format. Of course, you should instead return the result of your
     # search algorithm. Remember: if no solution is possible for a given input,
     # return `None` instead of a list.
-    
-    
-    
-    return [
-        MoveAction(Coord(0, 5), [Direction.Down]),
-        MoveAction(Coord(1, 5), [Direction.DownLeft]),
-        MoveAction(Coord(3, 3), [Direction.Left]),
-        MoveAction(Coord(3, 2), [Direction.Down, Direction.Right]),
-        MoveAction(Coord(5, 4), [Direction.Down]),
-        MoveAction(Coord(6, 4), [Direction.Down]),
-    ]
+
